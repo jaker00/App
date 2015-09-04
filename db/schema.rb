50e808ddd05a2596b6075b4a97665a9d98e4a5dc
Jake Reisner <jakereisner@Jakes-MacBook-Pro.local>
2015-09-03 20:51:56 -0500

    added cool sign-in form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903235856) do
+ActiveRecord::Schema.define(version: 20150904003412) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "content"
